e488317292825e266501119984bfde44ccfdb082
ivan1783 <ivan1783@users.noreply.github.com>
2014-11-08 17:43:16 +0100

    Update Main.ino
    Added acceleration/deceleration functions.

--- Main.ino
+++ Main.ino
@@ -1,6 +1,6 @@
 
 
-int numberOfRevolutions =16;
+int numberOfRevolutions =16;          // how many steps are between two feeder holes
 int stepsPerRevolution = 12;          // how many steps are in one revolution - set per motor type
 
 // Pin names to physical location
@@ -22,8 +22,8 @@ int limitSwitch = 9;
 int feed = 0;
 
 int stepTimeInput = 2;
-//optimal value for 12v on EM-319 motor is 3.3ms between steps
-float stepTime = 3.3;
+//optimal value for 12v on EM-319 motor is 2.4ms between steps
+float stepTime = 2500;
 
 //initial value for limit switch 8(allows home process to start
 int limitSwitchValue = 0;
@@ -42,6 +42,7 @@ int homed = 0;
 
 void setup()
 {
+
   //pin configuration
   pinMode(coilOne, OUTPUT); 
   pinMode(coilTwo, OUTPUT); 
@@ -65,10 +66,12 @@ void setup()
 
 void loop()
 {
-  //trigger motor to move
+
+  //check status of home button
   homeButtonValue = digitalRead(homeButton);
+  //check status of start button
   feed = digitalRead(startButton);
-  
+  //if the machined is not homed, set the home led to on, else set the homed led to off and turn the ready led on
   if (homed == 0) {
     digitalWrite(homeLED, HIGH);
   } else {
@@ -76,7 +79,7 @@ void loop()
      digitalWrite(readyLED, HIGH);
   }   
   delay(100);
-  
+  //home routine
   if (homeButtonValue == LOW) {
    SteppingHome(numberOfSteps, stepTime);
    homed = 1; 
@@ -84,7 +87,7 @@ void loop()
   else{
 
   }
-  
+  //feed routine, if the machine is homed and the start button is pressed start feeding
   if (feed == LOW && homed == 1) {
     digitalWrite(readyLED, LOW);
     feedLoop(position, feedRate, numberOfSteps, stepTime);
@@ -126,39 +129,79 @@ int feedLoop(int position, long feedRate, int numberOfSteps, float stepTime){
     digitalWrite(readyLED, LOW);
     digitalWrite(homeLED, HIGH);
 }
+//function SteppingHome is to find the home position of the machine
 int SteppingHome(int numberOfSteps, float stepTime){
 
   //turn on output stages
   digitalWrite(enable, HIGH);
   delay(10);
-
+  stepTime = stepTime + 3000;
+  int acceleration = 160;
+  int accelerationSteps = 40;
   //this is called when the "home" button is pressed, runs until limit switch is triggered
   while(limitSwitchValue == 0) {
+    
+    if (accelerationSteps > 0){
+      stepTime = stepTime - acceleration;
+      acceleration = acceleration - 4;
+      accelerationSteps--;
+    }
+    
     //step 1
     digitalWrite(coilOne, LOW);
     digitalWrite(coilTwo, LOW);
-    delay(stepTime);
+    delayMicroseconds(stepTime);
     //step 2
     digitalWrite(coilOne, HIGH);
     digitalWrite(coilTwo, LOW);
-    delay(stepTime);
+    delayMicroseconds(stepTime);
     //step 3
     digitalWrite(coilOne, HIGH);
     digitalWrite(coilTwo, HIGH);
-    delay(stepTime);
+    delayMicroseconds(stepTime);
     //step 4
     digitalWrite(coilOne, LOW);
     digitalWrite(coilTwo, HIGH);
-    delay(stepTime);
-    //turn off output stages
+    delayMicroseconds(stepTime);
+   
     limitSwitchValue = digitalRead(limitSwitch);
   }
+
+  
+int deccelerationSteps = 45;
+int decceleration = 4;
+
+   while(deccelerationSteps > 0) {
+    
+    if (deccelerationSteps > 0 ) {
+       stepTime = stepTime + decceleration;
+       decceleration = decceleration + 4;
+        deccelerationSteps--;
+    }
+    //step 1
+    digitalWrite(coilOne, LOW);
+    digitalWrite(coilTwo, LOW);
+    delayMicroseconds(stepTime);
+    //step 2
+    digitalWrite(coilOne, HIGH);
+    digitalWrite(coilTwo, LOW);
+    delayMicroseconds(stepTime);
+    //step 3
+    digitalWrite(coilOne, HIGH);
+    digitalWrite(coilTwo, HIGH);
+    delayMicroseconds(stepTime);
+    //step 4
+    digitalWrite(coilOne, LOW);
+    digitalWrite(coilTwo, HIGH);
+    delayMicroseconds(stepTime);
+  }
+  //turn off output stage
   digitalWrite(enable, LOW);
   //reset limit switch variable to 0 (SteppingHome can run again)
   limitSwitchValue = 0;
   //delay before moving SteppingBack in order to take up slack in feed carrier
-  delay(300);
-  SteppingBack(120, stepTime);
+  delay(100);
+  SteppingBack(170, 2500);
 }
 
 int SteppingBack(int numberOfSteps, float stepTime){
@@ -166,30 +209,44 @@ int SteppingBack(int numberOfSteps, float stepTime){
   //turn on output stages
   digitalWrite(enable, HIGH);
   delay(10);
-  
 
+  stepTime = stepTime + 3000;
+  unsigned int acceleration = 160;
+  unsigned int decceleration = 4;
+  
   for(numberOfSteps; numberOfSteps > 0; numberOfSteps--) {
     
     //step 4
     digitalWrite(coilOne, LOW);
     digitalWrite(coilTwo, HIGH);
-    delay(stepTime);
+    delayMicroseconds(stepTime);
     //step 3
     digitalWrite(coilOne, HIGH);
     digitalWrite(coilTwo, HIGH);
-    delay(stepTime);
+    delayMicroseconds(stepTime);
     //step 2
     digitalWrite(coilOne, HIGH);
     digitalWrite(coilTwo, LOW);
-    delay(stepTime);
+    delayMicroseconds(stepTime);
     //step 1
     digitalWrite(coilOne, LOW);
     digitalWrite(coilTwo, LOW);
-    delay(stepTime);
-
-    //turn off output stages
+    delayMicroseconds(stepTime);
+    
+    if (acceleration > 0) {
+    stepTime = stepTime - acceleration;
+    
+    acceleration = acceleration - 4;
+    }
+    
+    if (numberOfSteps < 45) {
+    stepTime = stepTime + decceleration;
+    
+    decceleration = decceleration + 4;
+    }
 
   }
+  //turn off output stages
   digitalWrite(enable, LOW);
   
 }
